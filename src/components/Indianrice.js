f9b95dc242a35075a67bede71ad647151617faee
arshibhuva <arshitabhuva@gmail.com>
2023-07-19 18:28:48 +0530

    form

--- src/components/Indianrice.js
+++ src/components/Indianrice.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import PhoneInput from 'react-phone-input-2';
+import { useLocation } from "react-router";
+import 'react-phone-input-2/lib/style.css';
 export default function Indianrice({props}) {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   const name = props[0].maintitle;
   console.log(name);
@@ -188,9 +195,206 @@ export default function Indianrice({props}) {
                     </div>
                   </div>
                   <div className="row">
+                  
                   <div id="basmati-btn-last" className="col-12 mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
                   <div className="brbtn2 ms-0">Yes! i am interested</div>
                 </div>
+                <div className="row" id="inner-form-full">
+                <div className="col-12">
+                  <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabindex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog ">
+                      <div className="modal-content">
+                        <div className="modal-body">
+                        <div className="scoll-tree">
+                          <div className="row" id="inner-box-form">
+                          <button type="button" class="btn-close  btn-close-model" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <div className="col col-form-img">
+                              <div className="back-img back-img-box" style={{background:`url(${data.img})`}}>
+                                <div className="bluer-back-img "></div>
+                                <img
+                                  className="product-img product-2"
+                                  src={data.img}
+                                  alt=""
+                                />
+                                <p className="title-interested">
+                                  {data.title}
+                                </p>
+                              </div>
+                              <div className='text-inner-form'>
+                      
+                                <p className="text-gray pt-2 m-0">
+                                  <b className="">Preferred Buyer From</b>
+                                </p>
+                                <p className="text-gray mt-0 m-0">
+                                  <span> Location :</span><span className="text-gray "> Worldwide</span>
+                                </p>
+                              </div>
+                            </div>
+                            <div className="col col-form">
+                              <p className="title-interested2">
+                                <b>Get a Quick Quote</b>
+                              </p>
+                              
+                              <form>
+                              <div id="inner-forn" className="inner-form">
+                              <div className="row form-row">
+                <div className="col-12 col form-box">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1">
+                      <label className="name">Name </label>
+                    </div>
+                    <div className="input-main-name">
+                      <input
+                        className="inpute-name-box1"
+                        type="name"
+                        placeholder="Name"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12 col">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1">
+                      <label className="name">Email</label>
+                    </div>
+                    <div className="input-main-name">
+                      <input
+                        className="inpute-name-box1"
+                        type="name"
+                        placeholder="Email"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row form-row">
+                <div className="col-12 col">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1">
+                      <label className="name">Mobile No.</label>
+                    </div>
+                    <div className="input-main-name mobail-box">
+
+                    <PhoneInput  className="icon-flag" country={'in'} />
+                     <input
+                        className="inpute-name-box1 number-box"
+                        type="text"
+                        placeholder="Enter Mobail no."
+                        required
+                      />
+                      
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12 col">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1 gray-text">
+                      <label className="name">Quantity</label>
+                    </div>
+                    <div className="input-main-name d-flex">
+                      <input
+                        className="inpute-name-box1 mr-3"
+                        type="name"
+                        placeholder="Estimated Quantity"
+                        required
+                      />
+                      <input
+                        className="inpute-name-box1"
+                        type="name"
+                        placeholder="Ton"
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row form-row form-row-radio m-0">
+                <div className="col-12 col col-radio p-0">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1 textarea-box-text">
+                      <label className="name">Purpose of Requirement</label>
+                    </div>
+                    <div className="input-main-name input-main-name-radio">
+                      <span className="radio-btn">
+                        <input
+                          className="fs"
+                          type="radio"
+                          name="1"
+                          placeholder="Name"
+                          required
+                        />
+                        <span className="radio-text">Reselling</span>
+                      </span>
+                      <span className="radio-btn">
+                        <input
+                          className="fs"
+                          type="radio"
+                          name="1"
+                          placeholder="Name"
+                          required
+                        />
+                        <span className="radio-text">End Use</span>
+                      </span>
+                      <span className="radio-btn">
+                        <input
+                          className="fs"
+                          type="radio"
+                          name="1"
+                          placeholder="Name"
+                          required
+                        />
+                        <span className="radio-text">Raw Material</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row form-row">
+                <div className="col-12 col">
+                  <div className="main-box1 d-flex">
+                    <div className="inpute-name1 textarea-box-text">
+                      <label className="name">Requirement Details</label>
+                    </div>
+                    <div className="input-main-name">
+                      <textarea
+                        className="inpute-name-box1 textarea-box"
+                        placeholder="I am interested. Kindly send the quotation for the same."
+                        name=""
+                        id=""
+                        cols="20"
+                        rows="3"
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row form-row" id="inner-btn-enquiry">
+                <div className="col-sm-12">
+                  <div id="box-btn">
+                    <div className="box-btn">Send Enquiry</div>
+                  </div>
+                </div>
+              </div>
+                              </div>
+                               
+                              </form>
+                            </div>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
                   </div>
               
                 </div>
